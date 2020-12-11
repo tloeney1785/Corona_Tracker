82af69e1dc1ff8653053ff0112da0cadd176df02
Tony Lee <tloeney1785@gmail.com>
2020-12-10 19:03:49 -0800

    implemented country state

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
 
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount(){
@@ -16,13 +17,18 @@ class App extends React.Component {
         this.setState({ data: fetchedData});
     }
 
+    handleCountryChange = async (country) => {
+        //fetch the data and set state
+        console.log(country);
+    }
+
     render(){
         const { data } = this.state;
 
         return(
             <div className={styles.container}>
                 <Cards data={data}/>
-                <CountryPicker />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart />
             </div>
         )
